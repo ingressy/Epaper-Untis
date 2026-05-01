46d2676df7d42cb2fa0854b47df8d7eb34a3a690
ingressy <j.czinzoll@ingressy.de>
2026-05-01 22:38:54 +0200

    4h werden zusammen

--- data.py
+++ data.py
@@ -13,20 +13,24 @@ def merge_stunden(stunden: list[Any] | None) -> list[Any] | None:
 
     merged = []
     current = stunden[0].copy()
+    current["anzahl"] = 1
 
     for next_h in stunden[1:]:
-        #prüft ob gleicher lehrer, fach, klasse und Endzeit == Startzeit
+        #prüft ob gleicher lehrer, fach, klasse und datum
         same = (
             current["teacher"] == next_h["teacher"] and
             current["subject"] == next_h["subject"] and
             current["klasse"] == next_h["klasse"] and
-            current["end_time"] == next_h["start_time"]
+            current["date"] == next_h["date"]
         )
         if same:
             current["end_time"] = next_h["end_time"]
+            current["anzahl"] += 1
         else:
             merged.append(current)
             current = next_h.copy()
+            current["anzahl"] = 1
+
     merged.append(current)
     return merged
 
